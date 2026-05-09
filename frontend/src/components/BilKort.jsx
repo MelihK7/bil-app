1bf837931a3eac3ead621c01c2adabcd5f48c0bd
Melih <m.kelkitli@hotmail.com>
2026-05-09 20:05:23 +0200

    Importeret funktionalitet der muliggøre at route fra side til side

--- frontend/src/components/BilKort.jsx
+++ frontend/src/components/BilKort.jsx
@@ -4,11 +4,19 @@ import './BilKort.css'
 function BilKort ({ bil })   {
     return (
         <div className="bil-kort">
-            <h2>{bil.mærke} {bil.model}</h2>
-            <p>Årgang: {bil.årgang}</p>
-            <p>Pris: {bil.pris} DKK</p>
-            <p>Km: {bil.km}</p>
-            <p>Type: {bil.type}</p>
+
+            <img src={bil.billeder[0]} alt={`${bil.mærke} ${bil.model}`} className="bil-thumbnail" />
+
+            <div className="bil-info">
+                <h2>{bil.mærke} {bil.model}</h2>
+                <p>Årgang: {bil.årgang}</p>
+                <p>Pris: {bil.pris.toLocaleString("da-DK")} DKK</p> // Formater efter tusind-separator for prisen
+                
+                <p>Km: {bil.km.toLocaleString("da-DK")}</p>
+                // Tilføj tusind-separator til km 
+                <p>Type: {bil.type}</p>
+            </div>
+
         </div>
     );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BroswerRouter} from 'react-router-dom' // Import der gør router funktionaliteten muligt mellem pages
 import './index.css'
 import App from './App.jsx'
 
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
+      <BroswerRouter>
+        <App />
+      </BroswerRouter>
+
   </StrictMode>,
 )
